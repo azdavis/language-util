c467a46aab8a11917db60f7fac2c14fc6994ef23
Ariel Davis <ariel.z.davis@icloud.com>
2021-03-02 20:43:23 -0800

    Add char-name

---
+++ crates/char-name/src/lib.rs
@@ -0,0 +1,37 @@
+//! Names for punctuation characters.
+
+#![deny(missing_debug_implementations)]
+#![deny(missing_docs)]
+#![deny(rust_2018_idioms)]
+
+/// Returns the name of the given character.
+pub fn get(c: char) -> Option<&'static str> {
+  let ret = match c {
+    '-' => "Minus",
+    ',' => "Comma",
+    ';' => "Semicolon",
+    ':' => "Colon",
+    '!' => "Bang",
+    '?' => "Question",
+    '.' => "Dot",
+    '(' => "LRound",
+    ')' => "RRound",
+    '[' => "LSquare",
+    ']' => "RSquare",
+    '{' => "LCurly",
+    '}' => "RCurly",
+    '*' => "Star",
+    '/' => "Slash",
+    '&' => "And",
+    '%' => "Percent",
+    '^' => "Carat",
+    '+' => "Plus",
+    '<' => "Lt",
+    '=' => "Eq",
+    '>' => "Gt",
+    '|' => "Bar",
+    '~' => "Tilde",
+    _ => return None,
+  };
+  Some(ret)
+}
--- readme.md
+++ readme.md
@@ -1,3 +1,7 @@
 # language-server-util
 
 Various utility crates that might be useful when implementing a language server.
+
+## `char-name`
+
+A helper function that gives an English name to some characters.
