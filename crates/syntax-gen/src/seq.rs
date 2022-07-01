8bc999385bb5842bd14a7ea89c0a0f186cc47b44
Ariel Davis <ariel.z.davis@icloud.com>
2022-06-30 22:35:02 -0700

    Use then_some

--- crates/syntax-gen/src/seq.rs
+++ crates/syntax-gen/src/seq.rs
@@ -36,7 +36,7 @@ pub(crate) fn get(cx: &Cx, name: Ident, rules: &[Rule]) -> TokenStream {
       }
 
       fn cast(node: SyntaxNode) -> Option<Self> {
-        (node.kind() == SK::#name).then(|| Self(node))
+        (node.kind() == SK::#name).then_some(Self(node))
       }
 
       fn syntax(&self) -> &SyntaxNode {
