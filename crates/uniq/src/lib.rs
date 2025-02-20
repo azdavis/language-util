08ef17a1f91e82b50c18ede0a7c4208e5ae32106
Ariel Davis <ariel.z.davis@icloud.com>
2025-02-19 18:40:49 -0800

    Rename
    gen is reserved in Rust 2024

--- crates/uniq/src/lib.rs
+++ crates/uniq/src/lib.rs
@@ -14,24 +14,24 @@ impl fmt::Display for Uniq {
   }
 }
 
-/// A generator for [`Uniq`]s.
+/// A maker for [`Uniq`]s.
 #[derive(Debug, Default)]
-pub struct UniqGen(u32);
+pub struct UniqMk(u32);
 
-impl UniqGen {
+impl UniqMk {
   /// Returns a [`Uniq`] not equal to any other [`Uniq`] returned thus far from
-  /// this [`UniqGen`].
+  /// this [`UniqMk`].
   ///
   /// # Example
   ///
   /// ```
-  /// # use uniq::UniqGen;
-  /// let mut uniq_gen = UniqGen::default();
-  /// let u1 = uniq_gen.gen();
-  /// let u2 = uniq_gen.gen();
+  /// # use uniq::UniqMk;
+  /// let mut uniq_gen = UniqMk::default();
+  /// let u1 = uniq_gen.mk();
+  /// let u2 = uniq_gen.mk();
   /// assert_ne!(u1, u2);
   /// ```
-  pub fn gen(&mut self) -> Uniq {
+  pub fn mk(&mut self) -> Uniq {
     let ret = Uniq(self.0);
     // assuming overflow won't happen.
     self.0 += 1;
